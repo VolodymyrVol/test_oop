c9c75b8dddf3ae0e0bf37cced0b72146ab78d3e8
Vova <lololoskafry@gmail.com>
2024-05-21 11:20:16 +0300

    added basic calculator

--- Test_20.05.24/FileName.cpp
+++ Test_20.05.24/FileName.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+template<typename T, typename B>
+void Sum(T a, B b)
+{
+	cout << a + b << endl;
+}
+template<typename T, typename B>
+void Min(T a, B b)
+{
+	cout << a - b << endl;
+}
+template<typename T, typename B>
+void Degree(T a, B b)
+{
+	cout << a * b << endl;
+}
+template<typename T, typename B>
+void Dis(T a, B b)
+{
+	cout << a / b << endl;
+}
+
 int main()
 {
-
+	int a = 10;
+	double b = 20;
+	Sum(a, b);
+	Min(a, b);
+	Degree(a, b);
+	Dis(a, b);
 }
